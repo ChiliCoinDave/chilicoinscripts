62339248c2491ec7a0c08ac5a4b5301ab392ee89
ChiliCoinDave <38087331+ChiliCoinDave@users.noreply.github.com>
2018-04-05 00:09:57 +0100

    Create chilicoin1.sh

---
+++ masternode/chilicoin1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget -O /root/.chilicoin-v2/masternode.conf https://raw.githubusercontent.com/viscolex/ScriptRepository/master/crypto/ams/masternode.conf
+echo "Copy the contents from your local Masternode.conf to the VPS"
+nano /root/.amsterdamcoin/masternode.conf
+echo "Copy the contents from your local amsterdamcoin.conf to the VPS"
+wget -O /root/.chilicoin-v2/amsterdamcoin.conf https://raw.githubusercontent.com/viscolex/ScriptRepository/master/crypto/ams/ams.conf
+nano /root/.amsterdamcoin/ams.conf
+./amsterdamcoind -daemon

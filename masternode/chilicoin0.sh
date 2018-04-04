ed152a39a8ce3e1d9f931ac0690e50cfd96cc2e9
ChiliCoinDave <38087331+ChiliCoinDave@users.noreply.github.com>
2018-04-05 00:36:41 +0100

    Update chilicoin0.sh

--- masternode/chilicoin0.sh
+++ masternode/chilicoin0.sh
@@ -8,6 +8,6 @@ cd chilicoin
 wget https://github.com/ChiliCoinProject/ChiliCoin/releases/download/v2.0.0.0/chilicoin-daemon-2.0.0.0-linux64.tar.gz
 tar xzvf chilicoin-daemon-2.0.0.0-linux64.tar.gz
 ./chilicoin -daemon
-wget https://raw.githubusercontent.com/viscolex/ScriptRepository/master/crypto/ams/ams1.sh
-chmod +x ams1.sh
-sudo bash ams1.sh
+wget https://raw.githubusercontent.com/ChiliCoinDave/chilicoinscripts/master/masternode/chilicoin1.sh
+chmod +x chilicoin1.sh
+sudo bash chilicoin1.sh

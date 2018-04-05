005c641579b02224946a08d33182747d5f566e00
ChiliCoinDave <38087331+ChiliCoinDave@users.noreply.github.com>
2018-04-05 23:58:17 +0100

    Update chilicoin1.sh

--- masternode/chilicoin1.sh
+++ masternode/chilicoin1.sh
@@ -4,5 +4,5 @@ echo "Copy the contents from your local Masternode.conf to the VPS"
 nano /root/.chilicoin-v2/masternode.conf
 echo "Copy the contents from your local chilicoin.conf to the VPS"
 wget -O /root/.chilicoin-v2/chilicoin.conf https://raw.githubusercontent.com/ChiliCoinDave/chilicoinscripts/master/masternode/chilicoin.conf
-nano /root/.chilicoin-v2/ams.conf
+nano /root/.chilicoin-v2/chilicoin.conf
 ./chilicoind -daemon

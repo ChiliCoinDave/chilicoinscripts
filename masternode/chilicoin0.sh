b5321604781929826c6be7a109d8519337d16e4a
ChiliCoinDave <38087331+ChiliCoinDave@users.noreply.github.com>
2018-04-06 00:01:16 +0100

    Update chilicoin0.sh

--- masternode/chilicoin0.sh
+++ masternode/chilicoin0.sh
@@ -7,7 +7,7 @@ mkdir chilicoin
 cd chilicoin
 wget https://github.com/ChiliCoinProject/ChiliCoin/releases/download/v2.0.0.2/chilicoin-daemon-2.0.0.2-linux64.tar.gz
 tar xzvf chilicoin-daemon-2.0.0.2-linux64.tar.gz
-./chilicoin -daemon
+./chilicoind -daemon
 wget https://raw.githubusercontent.com/ChiliCoinDave/chilicoinscripts/master/masternode/chilicoin1.sh
 chmod +x chilicoin1.sh
 sudo bash chilicoin1.sh
